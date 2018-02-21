e7e0e405705de4bd1ab2fe4a117a43c0306b68c8
SaltySkip <terentiev.andrei@gmail.com>
2018-02-21 19:07:03 +0900

    additional methods

---
+++ NeoSwift/Util/NEP9.swift
@@ -0,0 +1,19 @@
+//
+//  NEP9.swift
+//  NeoSwift
+//
+//  Created by Andrei Terentiev on 2/21/18.
+//  Copyright © 2018 drei. All rights reserved.
+//
+
+import Foundation
+import Neoutils
+
+public class NEP9 {
+    public typealias NEP9 = NeoutilsSimplifiedNEP9
+    public func parse(uri: String) -> NEP9? {
+        var error: NSError?
+        guard let uri = NeoutilsParseNEP9URI(uri, &error) else { return nil }
+        return uri
+    }
+}
