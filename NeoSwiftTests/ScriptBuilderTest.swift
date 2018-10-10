636208848483001ac11b373bbe44286ade78dda6
Koba <ricardoak25@gmail.com>
2018-10-10 12:10:19 -0300

    First draft of contractParameter

--- NeoSwiftTests/ScriptBuilderTest.swift
+++ NeoSwiftTests/ScriptBuilderTest.swift
@@ -72,7 +72,63 @@ class ScriptBuilderTest: XCTestCase {
         print (neoScript.rawBytes.fullHexString)
         #endif
         assert(neoScript.rawBytes.fullHexString == "51")
-        
-        
+    }
+    
+    func testContract() {
+        let testCases: [[String: Any?]] =
+            [["script": "00c1046e616d65675f0e5a86edd8e1f62b68d2b3f7c0a761fc5a67dc",
+              "scriptHash": "dc675afc61a7c0f7b3d2682bf6e1d8ed865a0e5f",
+              "operation": "name",
+              "args": []
+                ],
+             [
+                "script": "000673796d626f6c6711c4d1f4fba619f2628870d36e3a9773e874705b",
+                "scriptHash": "5b7074e873973a6ed3708862f219a6fbf4d1c411",
+                "operation": "symbol",
+                "args": nil
+                ],
+             [
+                "script": "0008646563696d616c736711c4d1f4fba619f2628870d36e3a9773e874705b",
+                "scriptHash": "5b7074e873973a6ed3708862f219a6fbf4d1c411",
+                "operation": "decimals",
+                "args": false
+                ],
+             [
+                "script": "205fe459481de7b82f0636542ffe5445072f9357a1261515d6d3173c07c762743b51c10962616c616e63654f666711c4d1f4fba619f2628870d36e3a9773e874705b",
+                "scriptHash": "5b7074e873973a6ed3708862f219a6fbf4d1c411",
+                "operation": "balanceOf",
+                "args": ["5fe459481de7b82f0636542ffe5445072f9357a1261515d6d3173c07c762743b"]
+                ],
+             [
+                "script": "5767b7040c106561763ce38c0ce658a946e5d1b381db",
+                "scriptHash": "db81b3d1e546a958e60c8ce33c766165100c04b7",
+                "operation": nil,
+                "args": 7
+                ]
+//             [
+//                "script": "143775292229eccdf904f16fff8e83e7cffdc0f0ce51c10962616c616e63654f666711c4d1f4fba619f2628870d36e3a9773e874705b",
+//                "scriptHash": "5b7074e873973a6ed3708862f219a6fbf4d1c411",
+//                "operation": "balanceOf",
+//                "args": [
+//                    "type": "Hash160",
+//                    "value": "cef0c0fdcfe7838eff6ff104f9cdec2922297537"
+//                ]
+            ]
+        ]
+        let neoScript = ScriptBuilder()
+        for testCase in testCases {
+            guard let script = testCase["script"] as? String,
+                let scriptHash = testCase["scriptHash"] as? String else {
+                    fatalError("Coud not parse test cases")
+            }
+            let operation = testCase["operation"] as? String
+            let args = testCase["args"] ?? nil
+            neoScript.pushContractInvoke(scriptHash: scriptHash, operation: operation, args: args)
+            #if DEBUG
+            print (neoScript.rawBytes.fullHexString)
+            #endif
+            assert(neoScript.rawBytes.fullHexString == script)
+            neoScript.resetScript()
+        }
     }
 }
